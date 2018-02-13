0358bedc60fc5f26b5eed5712d0ee1212f18aedf
Unknown <prabhukumar.swamy@gmail.com>
2018-02-14 01:30:49 +0530

    final
    final

--- app/src/main/java/com/assignment/WidgetDemo.java
+++ app/src/main/java/com/assignment/WidgetDemo.java
@@ -14,43 +14,29 @@ import android.widget.Toast;
  */
 
 public class WidgetDemo extends AppWidgetProvider {
-    /*
-     *
-     * This method is called in response to the ACTION_APPWIDGET_UPDATE  from
-     * Override this method to implement own AppWidget functionality.
-     *
-     */
+    String url = "http://www.acadgild.com";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        Toast.makeText(context, "Widget Added/Updated", Toast.LENGTH_LONG).show();
-        String url = "http://www.acadgild.com";
-
-        for (int item = 0; item < appWidgetIds.length; item++) {
-            int currentId = appWidgetIds[item];
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-
-            //letting know the system that we would like this intent to be part of task Builder
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
-
-            //setting up pending intent
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            //setting up RemoteViews which will be used with pending intent
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_main);
-
-            //passing the id of button to pendingIntent's OnClick event
-            remoteViews.setOnClickPendingIntent(R.id.goBtn, pendingIntent);
-
-            // what is AppWidgetmanager?
-            //Updates AppWidget state; gets information about installed AppWidget providers and other AppWidget related state.
-
-            //what is updateAppWidget?
-            //Set the RemoteViews to use for all AppWidget instances for the supplied AppWidget provider.
-            appWidgetManager.updateAppWidget(currentId, remoteViews);
-            Toast.makeText(context, "Widget added successfully", Toast.LENGTH_SHORT).show();
-
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                 int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+        // Construct an Intent object includes web adresss.
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
 }
